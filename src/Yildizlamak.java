0d20eb265f598036faef99afa882486d44d96283
Orhun YILDIZ <orhunyyildiz@gmail.com>
2021-02-11 02:39:33 +0300

    Updated

--- src/Yildizlamak.java
+++ src/Yildizlamak.java
@@ -1,13 +1,12 @@
 public class Yildizlamak {
     public static void main(String[] args) {
-        stars("elalemseyfi");
+        stars("congratulations");
     }
 
     public static void stars(String string) {
         int length = string.length() + (string.length() - 1);
         char[] arr = new char[length];
-        arr[0] = string.charAt(0);
-        for (int i = 1; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             if (i % 2 == 0)
                 arr[i] = string.charAt(i / 2);
             else
